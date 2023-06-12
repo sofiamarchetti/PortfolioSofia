38b85603970ebc8db012ca6e662b0ac860655329
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-06-12 16:11:57 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,25 @@ let assunto = document.querySelector("#assunto")
 
 nome.style.width = "100%"
 email.style.width = "100%"
+
+function validaNome() {
+    let txt = document.querySelector("#txtNome")
+    if (nome.value.length < 3) {
+        txt.innerHTML = "Insira um nome válido"
+        txt.style.color = "red"
+    }else{
+        txt.innerHTML = "Nome válido"
+        txt.style.color = "green"
+    }
+}
+
+function validaEmail() {
+    let txt = document.querySelector("#txtNome")
+    if (nome.value.length < 3) {
+        txt.innerHTML = "Insira um nome válido"
+        txt.style.color = "red"
+    }else{
+        txt.innerHTML = "Nome válido"
+        txt.style.color = "green"
+    }
+}
